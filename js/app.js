36f68d2c0a1366cc36b98e76f27e4eaea3554692
Chema Sánchez <csanchez@barnatic.es>
2024-01-25 22:16:01 +0100

    Carrusel OK

--- js/app.js
+++ js/app.js
@@ -1,19 +1,7 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+// Inicializa carrusel de imágenes
+const myCarouselElement = document.querySelector('#carrusel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
