3c855bf0e459b05f3d5d3edba31ef015c3c661cf
Chema Sánchez <csanchez@barnatic.es>
2024-01-25 22:51:37 +0100

    Popovers OK

--- js/app.js
+++ js/app.js
@@ -5,3 +5,7 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 })
+
+// Inicializa popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
